18982aee9f78e66dd08b3ceac57796e9f696cae6
abhi061201 <abhishekbmdu1@gmail.com>
2023-09-09 20:07:56 +0530

    sattaking

--- lib/app/auth/controller/login_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class login_controller extends GetxController {
-  
-    TextEditingController mobilecontroller = TextEditingController();
-  TextEditingController password_controller = TextEditingController();
-}
